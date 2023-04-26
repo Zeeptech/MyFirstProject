0587ec63c5a7e97579ffcdc66ee250b3b86b6dcc
Zeeptech <Zeeptech@ZEEPTECHGAMING->
2023-04-26 12:01:55 +0200

    Add project files.

---
+++ MyFirstProject/Program.cs
@@ -0,0 +1,27 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Xml.Linq;
+
+namespace Program {
+    class Program
+    {
+        static void Main(string[] args) 
+        {
+            
+            
+            
+               
+            
+            
+
+        
+        
+        
+        
+        }
+        // Add Methods
+
+        
+    }
+
+   
+ }
