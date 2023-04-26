193048a7fa66414a6126f2a754c93d13f170fd4d
Zeeptech Gaming <ZeeptechGaming@hotmail.com>
2023-04-26 13:22:34 +0200

    Add a Player Class, Add a Method with Utilities.

--- MyFirstProject/Program.cs
+++ MyFirstProject/Program.cs
@@ -1,13 +1,18 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using MyFirstProject;
 using System.Xml.Linq;
+
 
 namespace Program {
     class Program
     {
         static void Main(string[] args) 
         {
-            
-            
+
+            Console.WriteLine("Hi and Welcome to my Game. Are you ready? \n Press any Button to Continue!\n");
+            Console.ReadKey();
+            Console.WriteLine("Firstly, I would need your name!\n What is your name:");
+            Console.ReadLine();
             
                
             
---
+++ MyFirstProject/Utilities.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyFirstProject
+{
+    public class Utilities
+    {
+        public static bool IsInArray(string[] arrays, string keyword)
+        {
+            bool exist = false;
+            if (Array.Exists(arrays, item => item.ToLower() == keyword.ToLower()))
+            {
+
+                exist = true;
+            }
+            return exist;
+
+
+        }
+    }
+}
