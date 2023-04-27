902a1e91cfcd68a69c5f9b743a2d655eb9c3988b
Zeeptech Gaming <ZeeptechGaming@hotmail.com>
2023-04-27 09:43:37 +0200

    Add variables to store the player information, Add Arrays for Races and Roles, Add a way to print all Variables in an Array

--- MyFirstProject/Program.cs
+++ MyFirstProject/Program.cs
@@ -8,11 +8,40 @@ namespace Program {
     {
         static void Main(string[] args) 
         {
+         string inputName;
+         string inputRace;
+         string inputGender;
+         string inputRole;
+            
+            
+         string[] AvaiableRaces = { "Orc", "HighElf", "Human" };
+         string[] AvaiableRoles = { "Warrior", "Mage", "Hunter" };
 
-            Console.WriteLine("Hi and Welcome to my Game. Are you ready? \n Press any Button to Continue!\n");
+
+
+           Console.WriteLine("Hi and Welcome to my Game. Are you ready? \n Press any Button to Continue!\n");
             Console.ReadKey();
             Console.WriteLine("Firstly, I would need your name!\n What is your name:");
-            Console.ReadLine();
+            inputName = Console.ReadLine();
+            Console.WriteLine($"{inputName} is it? Great! \nThen i would like to know what race you are!.\nThese are the avaiable Races:\n");
+                foreach (string race in AvaiableRaces)
+            {
+                Console.WriteLine(race);
+            }
+            Console.WriteLine("Which one will you Choose?");
+                inputRace = Console.ReadLine();
+
+            Console.WriteLine($"You have chosen: {inputRace} now thats a great Race!");
+            Console.WriteLine("Then i would like to know what gender you are. And Im sure you won't be happy, but for the sake of the game you are only able to select \"Male\" or \"Female\".");
+                inputGender = Console.ReadLine();
+
+            Console.WriteLine("Now thats the superior Gender.. Good choice!");
+            Console.WriteLine("Lastly i would like to know your Class. You can choose beween the following classes:");
+                foreach (string role in AvaiableRoles)
+            {
+                Console.WriteLine(role);
+            }
+                inputRole = Console.ReadLine();
             
                
             
