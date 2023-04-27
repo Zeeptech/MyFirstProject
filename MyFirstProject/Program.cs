9ab9ff9c9c3d445cc2cc43254d42d9b636aff96f
Zeeptech Gaming <ZeeptechGaming@hotmail.com>
2023-04-27 11:25:02 +0200

    Add Loops to check if the player input is correct to the allowed options for Race and Gender.

--- MyFirstProject/Program.cs
+++ MyFirstProject/Program.cs
@@ -1,5 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using MyFirstProject;
+using System.Data;
 using System.Xml.Linq;
 
 
@@ -12,31 +13,64 @@ namespace Program {
          string inputRace;
          string inputGender;
          string inputRole;
-            
+            bool validRace = false;
+            bool validGender = false;
             
          string[] AvaiableRaces = { "Orc", "HighElf", "Human" };
          string[] AvaiableRoles = { "Warrior", "Mage", "Hunter" };
-
+            string[] AvaiableGenders = { "Male", "Female" };
 
 
            Console.WriteLine("Hi and Welcome to my Game. Are you ready? \n Press any Button to Continue!\n");
             Console.ReadKey();
             Console.WriteLine("Firstly, I would need your name!\n What is your name:");
-            inputName = Console.ReadLine();
+                inputName = Console.ReadLine();
             Console.WriteLine($"{inputName} is it? Great! \nThen i would like to know what race you are!.\nThese are the avaiable Races:\n");
                 foreach (string race in AvaiableRaces)
             {
                 Console.WriteLine(race);
             }
+
             Console.WriteLine("Which one will you Choose?");
-                inputRace = Console.ReadLine();
 
-            Console.WriteLine($"You have chosen: {inputRace} now thats a great Race!");
+                while (!validRace)
+            {
+                Console.WriteLine("Please enter your race:");
+                string askInputRace = Console.ReadLine();
+                if (Utilities.IsInArray(AvaiableRaces, askInputRace!))
+                {
+                    inputRace = askInputRace;
+                    Console.WriteLine($"You have chosen: {inputRace} now that's a great Race!");
+                    validRace = true;
+                }
+                else
+                {
+                    Console.WriteLine($"The race '{askInputRace}' is not available.");
+                }
+            }
+
+
+
             Console.WriteLine("Then i would like to know what gender you are. And Im sure you won't be happy, but for the sake of the game you are only able to select \"Male\" or \"Female\".");
-                inputGender = Console.ReadLine();
+             
 
-            Console.WriteLine("Now thats the superior Gender.. Good choice!");
-            Console.WriteLine("Lastly i would like to know your Class. You can choose beween the following classes:");
+            while (!validGender)
+            {
+                Console.WriteLine("Please enter your gender:");
+                string askInputGender = Console.ReadLine();
+                if (Utilities.IsInArray(AvaiableGenders, askInputGender!))
+                {
+                    inputGender = askInputGender;
+                    Console.WriteLine($"You have chosen: {inputGender} Now thats the superior Gender.. Good choice!\n");
+                    validGender = true;
+                }
+                else
+                {
+                    Console.WriteLine($"Sorry. But there are ONLY TWO genders you Hippie, Please enter a VALID GENDER\n");
+                }
+            }
+
+            Console.WriteLine("Lastly i would like to know your Class. You can choose beween the following classes:\n");
                 foreach (string role in AvaiableRoles)
             {
                 Console.WriteLine(role);
