ade799417f0ad650add681bbd382f1a6e12262c8
Zeeptech Gaming <ZeeptechGaming@hotmail.com>
2023-05-03 12:25:43 +0200

    Create a Enemy subclass that inherits the Entity class Create the Attack array like the Roles subclasses

---
+++ MyFirstProject/Enemy.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyFirstProject
+{
+    class Enemy : Entity
+    {
+        public Attack[] attacks = {
+            new Attack("Test", "AttackTest", "FailTest", "CritTest"
+
+         ),
+            new Attack("Test", "AttackTest", "FailTest", "CritTest"
+
+         ),
+            new Attack("Test", "AttackTest", "FailTest", "CritTest")
+
+
+
+        };
+
+        public Enemy(string name, string gender, string race) : base(name, gender, race)
+        {
+
+
+        }
+    }
+}
