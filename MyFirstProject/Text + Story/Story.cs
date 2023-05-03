94f3c3f8ab52c779ca67bc14c4124f32eaa26197
Zeeptech Gaming <ZeeptechGaming@hotmail.com>
2023-05-03 12:38:38 +0200

    Add some story and structure of the story Create a class with Text and Story

---
+++ MyFirstProject/Text + Story/Story.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyFirstProject.Text___Story
+{
+    internal class Story
+    {
+        
+        /*
+        //Entry story
+            Console.WriteLine ($"Once upon a time, in the dark and mysterious land of Drakonia, a lone traveler made his way through the twisting forests and treacherous mountains. He was a {inputRole} of great skill, clad in black armor.");
+        
+               Console.WriteLine("The traveler had come to Drakonia seeking a fabled artifact of immense power, said to be guarded by three powerful guardians.")
+        
+        The journey had been long and perilous, but the warrior was determined to claim the artifact for himself.
+
+        // Before the First Fight
+        As he approached the entrance to the first guardian's lair, the warrior felt a chill run down his spine. The air was thick with the scent of death and decay, and the shadows seemed to writhe and twist like living things.
+
+        With his sword at the ready, the warrior stepped forward into the darkness. He was immediately set upon by a horde of undead warriors, their eyes glowing with an unholy light.
+
+        // After the First Fight
+        The first fight was fierce and bloody, but the warrior emerged victorious, his sword slick with the blood of his foes. With a sense of grim determination, he pressed on deeper into the guardian's lair.
+
+        // Before Second fight
+        The second guardian was a massive dragon, its scales gleaming in the flickering torchlight. The warrior could feel the heat of its breath on his face as it reared up to face him.
+
+        // After Second Fight
+        The second fight was even more intense than the first, with the warrior dodging and parrying the dragon's ferocious attacks. But his skill and determination won out in the end, and he struck the killing blow that sent the dragon crashing to the ground.
+
+        //Before Last Fight
+        The third and final guardian was the most fearsome of all. It was a twisted, demonic creature with glowing red eyes and a maw full of razor-sharp teeth. The warrior steeled himself for the battle to come.
+
+        // After Last Fight
+        The third fight was the most grueling of all, with the warrior and the demon locked in a brutal struggle that lasted for what seemed like hours. But in the end, the warrior emerged victorious, his sword plunged deep into the demon's heart.
+
+        //End line
+        With the guardians defeated, the warrior claimed the artifact for himself, a powerful amulet that would grant him untold power and riches beyond his wildest dreams. And so he emerged from the dark and gothic land of Drakonia, a hero forever remembered in legend and song.
+
+        */
+    }
+}
